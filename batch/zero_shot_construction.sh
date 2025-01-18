75c60955421ea847055da5aaa9266486bc26d582
Simon-Reif <simonreif@t-online.de>
2025-01-18 18:04:59 +0100

    zero shot stack script

--- batch/zero_shot_construction.sh
+++ batch/zero_shot_construction.sh
@@ -2,13 +2,13 @@
 
 # Sample Slurm job script for Galvani 
 
-#SBATCH -J zero_shot               # Job name
+#SBATCH -J zero_shot_stack               # Job name
 #SBATCH --ntasks=1                 # Number of tasks
 #SBATCH --cpus-per-task=8          # Number of CPU cores per task
 #SBATCH --nodes=1                  # Ensure that all cores are on the same machine with nodes=1
 #SBATCH --partition=2080-galvani   # Which partition will run your job
-#SBATCH --time=3-00:00             # Allowed runtime in D-HH:MM
-#SBATCH --gres=gpu:2               # (optional) Requesting type and number of GPUs
+#SBATCH --time=2-00:00             # Allowed runtime in D-HH:MM
+#SBATCH --gres=gpu:1               # (optional) Requesting type and number of GPUs
 #SBATCH --mem=50G                  # Total memory pool for all cores (see also --mem-per-cpu); exceeding this number will cause your job to fail.
 #SBATCH --output=/mnt/lustre/work/martius/mot363/cee-us/slurm-outputs/%x-%j.out       # File to which STDOUT will be written - make sure this is not on $HOME
 #SBATCH --error=/mnt/lustre/work/martius/mot363/cee-us/slurm-outputs/%x-%j.err        # File to which STDERR will be written - make sure this is not on $HOME
