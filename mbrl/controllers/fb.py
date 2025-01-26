a8400620ab9b9a43182a6d1009611dc5f7191baa
Simon-Reif <simonreif@t-online.de>
2025-01-26 03:18:14 +0100

    handling rollouts for pp estimation

--- mbrl/controllers/fb.py
+++ mbrl/controllers/fb.py
@@ -1,36 +1,41 @@
 import torch
 from torch.distributions.cauchy import Cauchy
+from abc import ABC, abstractmethod
 
-from mbrl.controllers import TrainableController
+from mbrl.controllers.abstract_controller import TrainableController
 from mbrl.models.fb_models import ForwardMap, BackwardMap
 from mbrl.rolloutbuffer import RolloutBuffer
 
 
 
-class ActionDiscretization:
-    def __init__(self, num_actions, action_low, action_high):
-        self.num_actions = num_actions
-        self.action_low = action_low
-        self.action_high = action_high
-
-    def get_action(self, action):
-        return action
-
+class ActionDiscretization(ABC):
+    def __init__(self):
+        pass
+    @abstractmethod
+    def num_actions(self):
+        pass
+    @abstractmethod
     def get_action_space_discrete(self):
-        return self.num_actions
-    
+        pass
+    @abstractmethod
     def get_action_space_continuous(self):
         return self.disc_to_cont(self.get_action_space_discrete)
-    
+    @abstractmethod
     def disc_to_cont(self, action):
         pass
-
-    def get_action_low(self):
-        return self.action_low
-
-    def get_action_high(self):
-        return self.action_high
     
+# settings from fb/controllable_agent/discrete_action_robots_modules/robots.py
+class FB_FetchReachActions(ActionDiscretization):
+    def __init__(self):
+        super().__init__()
+
+    def num_actions(self):
+        return self.action_space_discrete
+    def get_action_space_discrete(self):
+        return self.action_space_discrete
+    def disc_to_cont(self, action):
+        return action
+
 
 class ForwardBackwardController(TrainableController):
     def __init__(self, env, params, **kwargs):
@@ -45,11 +50,11 @@ class ForwardBackwardController(TrainableController):
         # TODO: preprocessing, change this
         self.obs_dim = env.observation_space.shape[0]
 
-        self.forward_network = ForwardMap(obs_dim=self.obs_dim, embed_dim=self.embed_dim, action_dim=self.action_discretization.num_actions)
+        self.forward_network = ForwardMap(obs_dim=self.obs_dim, embed_dim=self.embed_dim, action_dim=self.action_discretization.num_actions())
         self.backward_network = BackwardMap(obs_dim=self.obs_dim, embed_dim=self.embed_dim)
         # value 0.5 from paper
         self.cauchy = Cauchy(torch.tensor([0.0]), torch.tensor([0.5]))
-        self.forward_target_network = ForwardMap(obs_dim=self.obs_dim, embed_dim=self.embed_dim, action_dim=self.action_discretization.num_actions)
+        self.forward_target_network = ForwardMap(obs_dim=self.obs_dim, embed_dim=self.embed_dim, action_dim=self.action_discretization.num_actions())
         self.backward_target_network = BackwardMap(obs_dim=self.obs_dim, embed_dim=self.embed_dim)
         # load the weights into the target networks
         self.forward_target_network.load_state_dict(self.forward_network.state_dict())
@@ -75,6 +80,7 @@ class ForwardBackwardController(TrainableController):
     def set_backward_map(self, backward_map):
         self.backward_network = backward_map
     
+    # fits F, B to data from rollout_buffer
     def train(self, rollout_buffer):
         pass
 
@@ -84,8 +90,14 @@ class ForwardBackwardController(TrainableController):
     def policy_parameter_estimation(self, env, rollout_buffer, start_from_current_zr=False):
         #TODO: fit this to rollout_buffer specification
         with torch.no_grad():
+            obs=torch.tensor(rollout_buffer.as_array("observations"))
+            obs=torch.flatten(obs, end_dim=1)
+            b_obs=self.backward_network(obs)
+            r_obs=
             sum_representations = torch.sum([self.backward_network(obs) * env.compute_reward(obs) for obs in range(rollout_buffer.flat)], dim=0)
             if start_from_current_zr:
+                if self.z_r is None:
+                    self.z_r = 0
                 self.z_r = (self.n_zr_estim_samples*self.z_r + sum_representations)/ (self.n_zr_estim_samples + rollout_buffer.size())
                 self.n_zr_estim_samples += rollout_buffer.size()
             else:
@@ -108,6 +120,10 @@ class ForwardBackwardController(TrainableController):
             action_discrete=torch.argmax([self.forward_network(obs, a, z_r) for a in range(self.action_discretization.get_action_space_continuous())])
             return self.action_discretization.disc_to_cont(action_discrete)
         
+'''
+RolloutBuffer:
+list of rollouts : dict of obs, act, etc : list of transitions : dimension of obs, act, etc
+'''
 
 # test loading, saving
 # test input, output dims
