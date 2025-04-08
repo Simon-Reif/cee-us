337c8d1551f3ba2f54cab5b7d3f14c606acad52a
Simon-Reif <simonreif@t-online.de>
2025-04-08 22:04:50 +0200

    utils to combine buffers

--- mbrl/offline_helpers/buffer_utils.py
+++ mbrl/offline_helpers/buffer_utils.py
@@ -1,4 +1,8 @@
 import copy
+import pickle
+import os
+
+
 
 from mbrl.environments.abstract_environments import MaskedGoalSpaceEnvironmentInterface
 from mbrl.rolloutbuffer import Rollout, RolloutBuffer
@@ -17,11 +21,50 @@ def get_buffer_wo_goals(buffer:RolloutBuffer, env: MaskedGoalSpaceEnvironmentInt
         new_rollouts.append(new_rollout)
     return RolloutBuffer(rollouts=new_rollouts)
 
+def combine_buffers(buffer_1, buffer_2):
+    new_rollouts = copy.deepcopy(buffer_1.rollouts)
+    new_rollouts.extend(buffer_2.rollouts)
+    # new_rollouts is "_CustomList" type, so we need to convert it to a list
+    new_buffer = RolloutBuffer(rollouts=new_rollouts._list)
+    return new_buffer
+
+def combine_buffers_from_dirs(path_1, path_2, path_combined):
+    with open(path_1, 'rb') as f:
+        buffer_1 = pickle.load(f)
+    with open(path_2, 'rb') as f:
+        buffer_2 = pickle.load(f)
+    new_buffer = combine_buffers(buffer_1, buffer_2)
+    dir = os.path.dirname(path_combined)
+    if not os.path.exists(dir):
+        os.makedirs(dir)
+    with open(path_combined, "wb") as f:
+        pickle.dump(new_buffer, f)
+    return new_buffer
+
+
+# freeplay and planners return rollout buffers with different types
+# to combine them the type of "successes" has to be changed
+def repair_dtype_bug(buffer, save_path=None):
+    rollouts = buffer.rollouts
+    field_names = buffer[0].field_names()
+    new_rollouts = []
+    for rollout in rollouts:
+        new_dict = {field_name: copy.deepcopy(rollout[field_name]) for field_name in field_names}
+        new_dict["successes"] = rollout["successes"].squeeze(axis=1)
+        new_rollout = Rollout.from_dict(**new_dict)
+        new_rollouts.append(new_rollout)
+    new_buffer = RolloutBuffer(rollouts=new_rollouts)
+    if save_path is not None:
+        dir = os.path.dirname(save_path)
+        if not os.path.exists(dir):
+            os.makedirs(dir)
+        with open(save_path, "wb") as f:
+            pickle.dump(new_buffer, f)
+    return new_buffer
+
 
 if __name__=="__main__":
-    import os
     import numpy as np
-    import pickle
     import smart_settings
     from mbrl.environments import env_from_string
     env_name = "construction" # construction or "playground"
