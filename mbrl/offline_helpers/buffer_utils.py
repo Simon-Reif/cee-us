e9e8fa6117e4f9b22b1c6396f18f178c32258d83
Simon-Reif <simonreif@t-online.de>
2025-04-14 02:43:33 +0200

    combining buffers

--- mbrl/offline_helpers/buffer_utils.py
+++ mbrl/offline_helpers/buffer_utils.py
@@ -21,24 +21,44 @@ def get_buffer_wo_goals(buffer:RolloutBuffer, env: MaskedGoalSpaceEnvironmentInt
         new_rollouts.append(new_rollout)
     return RolloutBuffer(rollouts=new_rollouts)
 
-def combine_buffers(buffer_1, buffer_2):
+
+def load_buffer_wog(params):
+    wog_path=os.path.join(params.training_data_dir,'rollouts_wog')
+    if os.path.exists(wog_path):
+        print("Loading existing buffer without goals")
+        with open(os.path.join(params.training_data_dir, 'rollouts_wog'), 'rb') as f:
+            buffer = pickle.load(f)
+    else:
+        print("Extracting observations without goals and saving buffer")
+        with open(os.path.join(params.training_data_dir, 'rollouts'), 'rb') as f:
+            raw_buffer = pickle.load(f)
+        #TODO: pick out obs without goals, save
+        env = env_from_string(params.env, **params.env_params)
+        buffer = get_buffer_wo_goals(raw_buffer, env)
+        with open(wog_path, "wb") as f:
+                    pickle.dump(buffer, f)
+    return buffer
+    
+
+def combine_buffers(buffer_1, buffer_2, save_path=None):
     new_rollouts = copy.deepcopy(buffer_1.rollouts)
     new_rollouts.extend(buffer_2.rollouts)
     # new_rollouts is "_CustomList" type, so we need to convert it to a list
     new_buffer = RolloutBuffer(rollouts=new_rollouts._list)
+    if save_path is not None:
+        dir = os.path.dirname(save_path)
+        if not os.path.exists(dir):
+            os.makedirs(dir)
+        with open(save_path, "wb") as f:
+            pickle.dump(new_buffer, f)
     return new_buffer
 
-def combine_buffers_from_dirs(path_1, path_2, path_combined):
+def combine_buffers_from_dirs(path_1, path_2, path_combined=None):
     with open(path_1, 'rb') as f:
         buffer_1 = pickle.load(f)
     with open(path_2, 'rb') as f:
         buffer_2 = pickle.load(f)
-    new_buffer = combine_buffers(buffer_1, buffer_2)
-    dir = os.path.dirname(path_combined)
-    if not os.path.exists(dir):
-        os.makedirs(dir)
-    with open(path_combined, "wb") as f:
-        pickle.dump(new_buffer, f)
+    new_buffer = combine_buffers(buffer_1, buffer_2, save_path=path_combined)
     return new_buffer
 
 
@@ -64,20 +84,31 @@ def repair_dtype_bug(buffer, save_path=None):
 
 
 if __name__=="__main__":
-    import numpy as np
+    filename="rollouts_wog"
     import smart_settings
     from mbrl.environments import env_from_string
-    env_name = "construction" # construction or "playground"
-    working_dir = f'results/cee_us/{env_name}/2blocks/gnn_ensemble_cee_us_freeplay'
-
+    working_dir = f'results/cee_us/construction/2blocks/gnn_ensemble_cee_us_freeplay'
     params = smart_settings.load(os.path.join(working_dir, 'settings.json'), make_immutable=False)
     env = env_from_string(params.env, **params["env_params"])
-    with open(os.path.join(working_dir, 'checkpoints_225/rollouts'), 'rb') as f:
-        buffer = pickle.load(f)
+    with open(os.path.join(working_dir, 'checkpoints_225/rollouts_wog'), 'rb') as f:
+        buffer_freeplay = pickle.load(f)
 
-    print("Buffer contains {} rollouts, each with length {}".format(len(buffer), buffer[0]["observations"].shape[0]))
-
-    print(buffer[0]["observations"].shape)
-
-    buffer_wog=get_buffer_wo_goals(buffer, env)
-    print(buffer_wog[0].shape)
+    buffer_dirs = [
+        "results/cee_us/zero_shot/2blocks/225iters/construction_throw/gnn_ensemble_icem/checkpoints_000",
+        "results/cee_us/zero_shot/2blocks/225iters/construction_pp/gnn_ensemble_icem/checkpoints_000",
+        "results/cee_us/zero_shot/2blocks/225iters/construction_stack/gnn_ensemble_icem/checkpoints_000"]
+    target_dirs = [
+        "datasets/construction/fb/freeplay_plus_throw",
+        "datasets/construction/fb/freeplay_plus_pp",
+        "datasets/construction/fb/freeplay_plus_stack"]
+    buffer_paths = [os.path.join(buffer_dir, filename) for buffer_dir in buffer_dirs]
+    target_paths = [os.path.join(target_dir, filename) for target_dir in target_dirs]
+    
+    for buffer_path, target_path in zip(buffer_paths, target_paths):
+        with open(buffer_path, 'rb') as f:
+            buffer = pickle.load(f)
+        buffer = get_buffer_wo_goals(buffer)
+        buffer = repair_dtype_bug(buffer, save_path=buffer_path)
+        buffer = combine_buffers(buffer_freeplay, buffer, save_path=target_path)
+        print("Combined buffer contains {} rollouts, each with length {}".format(len(buffer), buffer[0]["observations"].shape[0]))
+        print(f"Combined buffer saved to {target_path}")
