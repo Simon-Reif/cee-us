e8e6537d2f2f17b7b271a0f826c40418cdc921fe
Simon-Reif <simonreif@t-online.de>
2025-05-26 19:58:16 +0200

    new sweep id in agent

--- batch/sweeps/start_agent.py
+++ batch/sweeps/start_agent.py
@@ -2,5 +2,5 @@ import wandb
 
 if __name__ == "__main__":
     wandb.login(key="25ee8d2e5fab3f028de5253bacadfe1ae8bfb760")
-    #wandb.agent(sweep_id="srtea/cee-us/ccdrxre2", count=1)
-    wandb.agent(sweep_id="srtea/test/q0hxccgi", count=1)
+    wandb.agent(sweep_id="srtea/cee-us/msdm6ilz", count=1)
+    #wandb.agent(sweep_id="srtea/test/q0hxccgi", count=1)
