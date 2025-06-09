fixing bugs bc main and normalizing

mbrl/offline_helpers/checkpoints.py
@@ -45,25 +45,16 @@ def get_success_rates_dict(working_dir, iter):
     success_rates = np.load(os.path.join(_get_cp_dir(working_dir, iter), "success_rate_dict.npy"))
     return success_rates.item()
 
-def get_zr_dict(working_dir, iter=None):
-    if iter is not None:
-        path = _get_cp_dir(working_dir, iter)
-    else:
-        path = working_dir
-    zrs = np.load(os.path.join(path, "z_r_dict.npy"), allow_pickle=True)
+def get_zr_dict(working_dir=None, iter=None, cp_dir=None):
+    cp_dir = cp_dir if cp_dir is not None else _get_cp_dir(working_dir, iter)
+    zrs = np.load(os.path.join(cp_dir, "z_r_dict.npy"), allow_pickle=True)
     return zrs.item()
 
-def get_goals_dict(working_dir, iter=None):
-    if iter is not None:
-        path = _get_cp_dir(working_dir, iter)
-    else:
-        path = working_dir
-    goals = np.load(os.path.join(path, "goals.npy"), allow_pickle=True)
+def get_goals_dict(working_dir=None, iter=None, cp_dir=None):
+    cp_dir = cp_dir if cp_dir is not None else _get_cp_dir(working_dir, iter)
+    goals = np.load(os.path.join(cp_dir, "goals.npy"), allow_pickle=True)
     return goals.item()
 
-# B(s_next) for B at iter and the offline training buffer
-def get_bs(working_dir, iter):
-    return np.load(os.path.join(_get_cp_dir(working_dir, iter), "bs.npy"), allow_pickle=True)
 
 # we assume parameters are saved in working_dir/
 def get_fb_controller(working_dir=None, iter=None, cp_dir=None, cpr=False):
@@ -80,8 +71,24 @@ def get_fb_controller(working_dir=None, iter=None, cp_dir=None, cpr=False):
     # else:
     return ForwardBackwardController.load(cp_dir, params.controller_params)
 
-def mean_success_rates_to_csv(working_dir, iter):
-    pass
+def load_checkpoint(cp_dir=None, working_dir=None, iter=None):
+    cp_dir = cp_dir if cp_dir is not None else _get_cp_dir(working_dir, iter)
+    working_dir = os.path.dirname(cp_dir) if working_dir is None else working_dir
+    params = smart_settings.load(os.path.join(working_dir, 'settings.json'), make_immutable=False)
+    controller = get_fb_controller(cp_dir=cp_dir)
+    return_dict = {"fb_controller": controller, "params": params}
+    try:
+        zr_dict = get_zr_dict(cp_dir=cp_dir)
+        return_dict["zr_dict"] = zr_dict
+    except FileNotFoundError:
+        print(f"Warning: No z_r_dict found in {cp_dir}.")
+    try:
+        goal_dict = get_goals_dict(cp_dir=cp_dir)
+        return_dict["goal_dict"] = goal_dict
+    except FileNotFoundError:
+        print(f"Warning: No goals found in {cp_dir}.")
+    
+    return return_dict
 
 
 def get_latest_checkpoint(working_dir, cpr=False):
@@ -98,3 +105,8 @@ def save_meta(working_dir, iter):
 
 # TODO: loading parts of FB controller only: here or in controller class?
 # TODO: get latest checkpoint?
+
+# deprecated
+# B(s_next) for B at iter and the offline training buffer
+def get_bs(working_dir, iter):
+    return np.load(os.path.join(_get_cp_dir(working_dir, iter), "bs.npy"), allow_pickle=True)