8f30a13d624db401480a574c872ebe130c4e0fcb
Simon-Reif <simonreif@t-online.de>
2025-06-10 21:25:27 +0200

    fixed sweep

--- batch/sweeps/start_agent.py
+++ batch/sweeps/start_agent.py
@@ -3,7 +3,7 @@ import wandb
 if __name__ == "__main__":
     wandb.login(key="25ee8d2e5fab3f028de5253bacadfe1ae8bfb760")
     
-    wandb.agent(sweep_id="srtea/cee-us/74tcnmkd", count=1)
+    wandb.agent(sweep_id="srtea/cee-us/8d60ut7f", count=1)
 
     #wandb.agent(sweep_id="srtea/cee-us/u7u0y9el", count=1)
     #wandb.agent(sweep_id="srtea/cee-us/msdm6ilz", count=1)
