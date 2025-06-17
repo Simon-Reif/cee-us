c71a2bba67eb5e2e3c67b754dd10effff1c45c11
Simon-Reif <simonreif@t-online.de>
2025-06-17 15:22:53 +0200

    sweep5: params fg_only

--- batch/sweeps/start_agent.py
+++ batch/sweeps/start_agent.py
@@ -2,8 +2,10 @@ import wandb
 
 if __name__ == "__main__":
     wandb.login(key="25ee8d2e5fab3f028de5253bacadfe1ae8bfb760")
-    wandb.agent(sweep_id="srtea/cee-us/i9rlw389", count=1)
 
+    wandb.agent(sweep_id="srtea/cee-us/7dp4cmu2", count=1) # sweep5: final params fg_only
+
+    #wandb.agent(sweep_id="srtea/cee-us/i9rlw389", count=1) # sweep4: final params all
     #wandb.agent(sweep_id="srtea/cee-us/8d60ut7f", count=1)
     #wandb.agent(sweep_id="srtea/cee-us/u7u0y9el", count=1)
     #wandb.agent(sweep_id="srtea/cee-us/msdm6ilz", count=1)
